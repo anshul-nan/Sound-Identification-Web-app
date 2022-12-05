40ed8e89694887267434360a6701f2a52869d507
anshul-nan <105433706+anshul-nan@users.noreply.github.com>
2022-12-05 17:27:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/C31ccfoKN/model.json',modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
