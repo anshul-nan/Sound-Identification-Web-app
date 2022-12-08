e88b58b6470f0250d679ca245d84655a09c68315
anshul-nan <105433706+anshul-nan@users.noreply.github.com>
2022-12-08 17:42:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,51 @@ function modelReady()
 {
     classifier.classify(gotResults);
 }
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        r = Math.floor(Math.random()*255) + 1;
+        g = Math.floor(Math.random() * 255 )+1;
+        b = Math.floor(Math.random()* 255)+1;
+        
+        document.getElementById("result_label").innerHTML=  "I can hear - "+results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy - "+ (results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
+        document.getElementById("result_confidence").style.color="rgb("+r+","+g+","+b+")";
+        
+        img1 = document.getElementById("alien1");
+        img2 = document.getElementById("alien2");
+        img3 = document.getElementById("alien3");
+        img4 = document.getElementById("alien4");
+    if(results[0].label=="paper whistle"){
+        img1.src = 'aliens-01.gif';
+        img2.src = 'aliens-02.png';
+        img3.src = 'aliens-03.png';
+        img4.src = 'aliens-04.png';
+    }
+    else if(results[0].label == 'clapping')
+    {
+        img1.src = 'aliens-01.png';
+        img2.src = 'aliens-02.gif';
+        img3.src = 'aliens-03.png';
+        img4.src = 'aliens-04.png';
+    }
+    else if(results[0].label == 'banging')
+    {
+        img1.src = 'aliens-01.png';
+        img2.src = 'aliens-02.png';
+        img3.src = 'aliens-03.gif';
+        img4.src = 'aliens-04.png';
+    }
+    else{
+        img1.src = 'aliens-01.png';
+        img2.src = 'aliens-02.png';
+        img3.src = 'aliens-03.png';
+        img4.src = 'aliens-04.gif';
+    }
+    }
+}
